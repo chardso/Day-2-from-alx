7a6116231f6207069716f5e23e96a531c6d7e9f7
chardso <wafelaandrew2@gmail.com>
2022-11-12 17:01:17 +0300

    my 0-memset.c

---
+++ 0x02-static_libraries/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * memset -fills memory with aconstant byte
+ * @s: pointer of the memory location where the memory will be set.
+ * @b: constant byte
+ * @n: byte of the memory area pointed by s.
+ * Return:0
+ * /
+   char *_ memset(char *s,char b,unsigned int n)
+{
+          unsigned int i = 0;
+          /*while i is less than n bytes*/
+          while (i <)
+           {
+                 /*b will be copied to the corresponding memory block*/
+                 s[i] = b;
+                 i++;
+           }
+           return (s);     
+                    
+           
+           
+
+           
