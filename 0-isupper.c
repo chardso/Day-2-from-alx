6dcd748a3e7297a7ffdd54ec52b2e3bfff35a9eb
chardso <wafelaandrew2@gmail.com>
2022-11-12 16:32:09 +0300

    my modified 0-isupper.c

---
+++ 0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isupper - checks for an uppercase letter
+ * @c: int to be checked
+ * return: 1 if c is uppercase,otherwise return 0
+ */
+
+int _issuper(int c)
+{
+          if (c >= 'A' && C <= 90)
+           {
+                    return (1)
+           }
+           else
+           {
+                    return (0);
+           }
+
+}
+           
+
